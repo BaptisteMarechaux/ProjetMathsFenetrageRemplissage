74dc5edee720e0079a7fbf0460b7c2d15053f092
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-01-07 23:36:08 +0100

    Clipping BV2

--- ProjetMathsFenetrageRemplissage/Clipping.h
+++ ProjetMathsFenetrageRemplissage/Clipping.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <vector>
+
 #include "libs\glm\glm.hpp"
+
 
 bool visible(const glm::vec2& checkedPoint, const glm::vec2& windowPointA, const glm::vec2& windowPointB);
 
 bool cuts(glm::vec2& s, glm::vec2& ss, glm::vec2& f, glm::vec2& f2);
 
 glm::vec2 intersection(const glm::vec2& checkedPointA, const glm::vec2& checkedPointB, const glm::vec2& windowPointA, const glm::vec2& windowPointB);
+
+std::vector<glm::vec2> maskInWindow(std::vector<glm::vec2>& s, std::vector<glm::vec2>& f);
+
